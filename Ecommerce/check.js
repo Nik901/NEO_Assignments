a9c6ff554114f8798abb6d204e40a594f6d2d9fa
Ankita Jadhav <ankita.jadhav295@gmail.com>
2021-01-07 19:13:58 +0530

    first commit

---
+++ Ecommerce/check.js
@@ -0,0 +1,30 @@
+function  fname(e){
+
+    if(e.length===0)
+    {
+    alert('Pls Enter field')
+    }
+}
+function  phone1(e){
+
+    if(e.length!==10)
+    {
+    alert('Pls enter 10 digit number')
+    }
+}
+
+function  zip(e){
+
+    if(e.length!==6)
+    {
+    alert('Pls Enter correct zip code !!')
+    }
+}
+function  email(e){
+
+    if(e.length===0)
+    {
+    alert('Pls Enter field')
+    }
+
+}
